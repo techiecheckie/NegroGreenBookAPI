10ac4b7fbd82b22a99dafe83d50c3a4f256d4d86
Riley <techiecheckie@gmail.com>
2018-07-03 20:01:28 -0500

    Initial commit for NegroGreenBookAPI.

---
+++ src/main/groovy/greenbookapi/NegroGreenPopApplication.groovy
@@ -0,0 +1,12 @@
+package greenbookapi
+
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NegroGreenPopApplication {
+    static void main(String[] args) {
+        SpringApplication.run NegroGreenPopApplication.class, args
+    }
+}
